53d2f6cc0dca80d03a52b79a23c58e43608ed429
Cristobal Medina Meza <cristobalmedina@meta.com>
2024-06-20 15:26:24 -0700

    added add card function

--- frontend/src/components/jsx/Board.jsx
+++ frontend/src/components/jsx/Board.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import HandleNewCard from './HandleNewCard'
 
 const Board = () => {
     const protocol = window.location.pathname;
-    
+    const [Form, setForm] = useState(false);
+    const [cards, setCards] = useState([]);
+
     useEffect(() => {
       fetchCards();
     }, []);
@@ -11,14 +14,49 @@ const Board = () => {
     const fetchCards = () => {
         fetch(`http://localhost:3000${protocol}`)
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => setCards(data))
             .catch(error => console.error('Error fetching:', error))
     }
     
+    const toggleForm = () => {
+      setForm(!Form);
+    };
+
+    const createCard = () => {
+      fetchCards();
+      setForm(false);
+    };
+
+    const renderCards = () => {
+      let filteredBoards = cards;
+  
+      return filteredBoards.map((board) => (
+        <div key={board.card_id} className="board">
+          <img src={board.gif} alt={board.name}/>
+          <h3>{board.message}</h3>
+          <p>{board.author}</p>
+
+          <button className="delete-card" onClick={() => deleteCard(board.id)}>
+            Delete Card
+          </button>
+        </div>
+      ));
+  };
 
     return (
       <div>
-        <p>hi </p>
+        <header className='App-header'>
+                <h2 className='Title'>KudoBoard</h2>
+        </header>
+
+        <div>
+          <button onClick={toggleForm}>
+              Create a New Card
+          </button>
+          {Form && (<HandleNewCard onSuccess={createCard} onClose={toggleForm} path={protocol}/>)}
+        </div>
+
+        <section className="board-grid">{renderCards()}</section>
       </div>
     );
   };
---
+++ frontend/src/components/jsx/HandleNewCard.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const NewCardForm = ({ onSuccess, onClose, path }) => {
+  const [newCardMessage, setNewCardMessage] = useState("");
+  const [newCardAuthor, setNewCardAuthor] = useState("");
+  
+  
+
+  const createNewCard = async () => {
+    try {
+      if (!newCardMessage || !newCardAuthor ) {
+        alert("Fill out all fields");
+        return; 
+      }
+
+      var path2 = path.slice(1);
+      var cardId = path2.substring(path2.indexOf('/') + 1);
+      console.log(cardId)
+
+
+      await axios.post(`http://localhost:3000${path}`, {
+        message: newCardMessage,
+        author: newCardAuthor,
+        gif: "https://media0.giphy.com/media/aoxmfNwLnMNr7C0wEE/giphy-downsized.gif?cid=72ae070ckfn1zyn2fi35os1aiqrgpu022y7qvg5efsnh9n9g&ep=v1_gifs_search&rid=giphy-downsized.gif&ct=g",
+        votes: 0,
+        board_id: cardId
+      });
+
+      onSuccess();
+
+      setNewCardMessage("");
+      setNewCardAuthor("");
+
+      onClose();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="new-card">
+        <button className="close" onClick={onClose}>
+          x
+        </button>
+        <h2>Create a New Card</h2>
+        <label>Title:</label>
+        <input
+          type="text"
+          value={newCardMessage}
+          onChange={(e) => setNewCardMessage(e.target.value)}
+          required
+        />
+
+        <label>Author:</label>
+        <input
+          type="text"
+          value={newCardAuthor}
+          onChange={(e) => setNewCardAuthor(e.target.value)}
+          required
+        />
+        
+
+        
+        <button className="submit" onClick={createNewCard}>
+          Create Card
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NewCardForm;
